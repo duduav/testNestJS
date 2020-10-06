0d79b74872deb24cad5d6e10c9c583d30ebf3bea
davinoam <dudu.avinoam@allscripts.com>
2020-10-06 20:12:37 +0300

    adjusting query for update

--- src/Dog/app.controller.ts
+++ src/Dog/app.controller.ts
@@ -1,5 +1,6 @@
 
-import { Controller, Get, Param, Post } from '@nestjs/common';
+
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { UserData } from './dog.schema';
 
@@ -7,9 +8,9 @@ import { UserData } from './dog.schema';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Post()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post( )
+  async getHello(@Body() userData: UserData): Promise<any> {
+    return this.appService.upsertData(userData);
   }
   @Get("/:id")
   async findAll(@Param('id') id): Promise<any> {
--- src/Dog/app.service.ts
+++ src/Dog/app.service.ts
@@ -1,5 +1,6 @@
 
 
+
 import { Injectable } from '@nestjs/common';
 import { MealsModel, UserData } from './dog.schema';
 import { Model } from 'mongoose';
@@ -8,89 +9,33 @@ import { Model } from 'mongoose';
 export class AppService {
   constructor(){
 
-  }  
-  
-  getData(id: string ): Promise<any>{
-    let result:any;
+  }
+
+  getData(id: string ): Promise<any>{    
     var promise = new Promise(function(resolve, reject) {
-      MealsModel.findOne({ 'id': '053061537' }, function (err, data) {
+      MealsModel.findOne({ 'id': id }, function (err, data) {
         if (err) {
           reject(err);
-        };        
+        };
         resolve(data);
       });
-  });
-    // MealsModel.findOne({ 'id': '053061537' }, function (err, data) {
-    //   if (err) {};
-    //   // Prints "Space Ghost is a talk show host".
-    //   console.log('%s %s is a %s.', data);
-    //   result =data
-    // });
+  });    
     return promise
   }
 
-  getHello(): string {
-    
-    let x = new MealsModel({
-      "id": "05306153",
-      "name": "דודו אבינועם",
-      "dates": {
-          "10/5/2020": {
-              "meals": [
-                  {
-                      "meal": [
-                          "חביתה",
-                          ""
-                      ],
-                      "header": "ארוחת בוקר"
-                  },
-                  {
-                      "meal": [
-                          "בשר",
-                          ""
-                      ],
-                      "header": "ארוחת צהרים"
-                  },
-                  {
-                      "meal": [
-                          "טונה",
-                          ""
-                      ],
-                      "header": "ארוחת ערב"
-                  }
-              ]
-          },
-          "10/4/2020": {
-              "meals": [
-                  {
-                      "meal": [
-                          "חביתה",
-                          ""
-                      ],
-                      "header": "ארוחת בוקר"
-                  },
-                  {
-                      "meal": [
-                          "בשר",
-                          ""
-                      ],
-                      "header": "ארוחת צהרים"
-                  },
-                  {
-                      "meal": [
-                          "טונה",
-                          ""
-                      ],
-                      "header": "ארוחת ערב"
-                  }
-              ]
-          }
-      }
-  }
+  upsertData(UserData :UserData): Promise<any> {
 
-    )
-    //let x = new DogModel({ 'name': 'sdsd'})
-    x.save();
-    return 'Hello World!';
+    var promise = new Promise(function(resolve, reject) {
+      MealsModel.replaceOne({'_id':UserData.id},UserData,{upsert:true},function (err, data) {
+        if (err) {
+          reject(err);
+        };
+        resolve(data);
+      });
+  });    
+    return promise
   }
+        
 }
+
+
